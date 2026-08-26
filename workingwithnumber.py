dbe14f9ac9daf155f96b230fa3c4e773be4354f5
Naaz parween <naazparween942@gmail.com>
2026-08-26 16:01:48 +0530

    Add basic number operations in workingwithnumber.py

---
+++ workingwithnumber.py
@@ -0,0 +1,12 @@
+# print(2+3.5)
+# print(3*4+8*7)
+# print(10%3)
+# print(10/3)
+from math import*
+my_num=5
+print(str(my_num)+" is my lucky number")
+print(pow(3,2))
+print(max(4,6))
+print(round(3.7))
+print(floor(3.7))
+print(ceil(3.7))
