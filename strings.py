0aee92d593ef56be20153081f7a65aaa37e03f6d
Naaz parween <naazparween942@gmail.com>
2026-08-26 16:00:34 +0530

    Add string manipulation examples in strings.py

---
+++ strings.py
@@ -0,0 +1,11 @@
+phrase="naaz parween"
+# print(phrase + " is good")
+# print(phrase.lower())
+print(phrase.upper().isupper())
+print(len(phrase))
+# 0 base indexing
+print(phrase[0])
+# //give index of "p"
+print(phrase.index("p"))
+# replace naaz to nazrin
+print(phrase.replace("naaz","nazrin"))
