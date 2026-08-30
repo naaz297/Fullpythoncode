3c3d82da82fd014a4b94706c559f23c76c367dc5
Naaz parween <naazparween942@gmail.com>
2026-08-30 14:07:00 +0530

    Create monthConversation dictionary
    Add a dictionary for month abbreviations to full names.

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+monthConversation={
+"Feb":"February",
+"Mar":"March",
+"Apr":"April",
+"May":"May",
+"Jun":"June",
+"Jul":"July",
+"Aug":"August",
+"Sep":"September",
+"Oct":"October",
+"Nov":"November",
+"Dec":"December",
+}
+print(monthConversation["Nov"])
