c1d56ae89cdb07f4bab5f7788b33e140bb9a02cd
Naaz parween <naazparween942@gmail.com>
2026-08-31 19:07:42 +0530

    Implement for loop with conditional printing

---
+++ forloop.py
@@ -0,0 +1,13 @@
+friends=["naaz","nazrin"]
+# len(friends)
+# for index in range(len(friends)):
+# # for index in range(3,10):
+# # for frriend in friends:
+#     # print(index)
+#     print(friends[index])
+for index in range(6):
+    if index==0:
+        print("first")
+    else:
+        print("not")
+  
