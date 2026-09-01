0d93151137da1a8140266f1d6169770b5f70ae32
Naaz parween <naazparween942@gmail.com>
2026-09-01 17:32:08 +0530

    Create readandappendfile.py

---
+++ readandappendfile.py
@@ -0,0 +1,7 @@
+# f=open("naaz.txt","r")
+# print(f.readline())  #readline read each line line is use for reading  single line
+# f.close()
+
+f=open("naaz.txt","a")
+f.write("I love to study")
+f.close()
