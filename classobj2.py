b442d1647b8d8e1a0a3b53bf676d0acffb160e01
Naaz parween <naazparween942@gmail.com>
2026-09-02 16:29:22 +0530

    Initialize Student objects and print details

---
+++ classobj2.py
@@ -0,0 +1,5 @@
+from classobj import Student
+
+student1=Student("naaz",20,201,True)
+student2=Student("nazrin",21,2002,False)
+print(student1.name,student1.age,student1.rollno,student1.is_on_probabation)
