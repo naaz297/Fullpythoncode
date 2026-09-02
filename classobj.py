5ccd9d0909ac8889a2960096fbfee12012deea16
Naaz parween <naazparween942@gmail.com>
2026-09-02 16:28:43 +0530

    Add Student class with initialization parameters

---
+++ classobj.py
@@ -0,0 +1,6 @@
+class  Student:
+    def __init__(self,name,age,rollno,is_on_probabation):
+        self.name=name
+        self.age=age
+        self.rollno=rollno
+        self.is_on_probabation=is_on_probabation
